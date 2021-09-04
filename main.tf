33ff92f2c467ae57239b72d1bca64c99b4a00bdd
mekkymayata <okpallannaemeka@gmail.com>
2021-09-04 08:23:03 +0100

    update

--- main.tf
+++ main.tf
@@ -21,21 +21,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
-}
-
-# virtual network
-resource "azurerm_virtual_network" "vnet" {
-  name = "myTFVnet"
-  address_space = [ "10.0.0.0/16" ]
-  location = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  tags = {
-    Environment = "dev"
-    Team = "DevOps"
-  }
-}
